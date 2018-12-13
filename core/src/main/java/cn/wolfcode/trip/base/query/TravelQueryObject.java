ee802926d3d646067a1095f7a99013379ace3086
xmm <1411580727@qq.com>
2018-12-13 21:43:22 +0800

    试试另外一种方法

--- core/src/main/java/cn/wolfcode/trip/base/query/TravelQueryObject.java
+++ core/src/main/java/cn/wolfcode/trip/base/query/TravelQueryObject.java
@@ -10,4 +10,5 @@ public class TravelQueryObject extends QueryObject {
     private Integer state;//状态
     private Boolean isPublic;//是否公开
     private Long travelId;//游记id
+    private String keyword;//关键字
 }
