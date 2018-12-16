7baa4b6ba240299b0fa4bba90acfedc8b561f1d9
tzq979696281 <tzq979696281@qq.com>
2018-12-16 13:09:03 +0800

    'sssss'

--- core/src/main/java/cn/wolfcode/trip/base/domain/NormalComment.java
+++ core/src/main/java/cn/wolfcode/trip/base/domain/NormalComment.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 import java.util.Date;
 @Getter@Setter
 public class NormalComment extends BaseDomain{
+
     //回复方
     private User user;
     //被回复方
@@ -16,11 +17,13 @@ public class NormalComment extends BaseDomain{
     private Date createTime;
     //类型
     private Integer type;
-    //主体id
+    //主体id 问答状态: 2
     private Long typeId;
     //最外层父节点
     private Long parentId;
     //标识
     private Long flagId;
+
+
 
 }
