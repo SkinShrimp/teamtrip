7a804dee4378c53c1d7d4a66e936ef1d3ea5d730
SkinShrimp <1353565431@qq.com>
2018-12-16 14:39:57 +0800

    朋友圈评论

--- core/src/main/java/cn/wolfcode/trip/base/domain/NormalComment.java
+++ core/src/main/java/cn/wolfcode/trip/base/domain/NormalComment.java
@@ -9,14 +9,14 @@ public class NormalComment extends BaseDomain{
     //回复方
     private User user;
     //被回复方
-    private User acceptuser;
+    private User acceptUser;
     //内容
     private String comment;
     //评论时间
     private Date createTime;
     //类型
     private Integer type;
-    //主体id
+    //主体id 2:代表朋友圈的评论
     private Long typeId;
     //最外层父节点
     private Long parentId;
--- core/src/main/java/cn/wolfcode/trip/base/service/impl/NormalCommentServiceImpl.java
+++ core/src/main/java/cn/wolfcode/trip/base/service/impl/NormalCommentServiceImpl.java
@@ -2,11 +2,14 @@ package cn.wolfcode.trip.base.service.impl;
 
 import cn.wolfcode.trip.base.domain.NormalComment;
 import cn.wolfcode.trip.base.mapper.NormalCommentMapper;
+import cn.wolfcode.trip.base.query.CircleForFriendsCommentQueryObject;
 import cn.wolfcode.trip.base.service.INormalCommentService;
+import cn.wolfcode.trip.base.util.UserContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.util.ArrayList;import java.util.Date;
+import java.util.List;import java.util.Map;
 @Service
 public class NormalCommentServiceImpl implements INormalCommentService {
 
@@ -15,6 +18,8 @@ public class NormalCommentServiceImpl implements INormalCommentService {
 
     @Override
     public int insert(NormalComment record) {
+        record.setUser(UserContext.getUser());
+        record.setCreateTime(new Date());
         return normalCommentMapper.insert(record);
     }
 
@@ -26,5 +31,11 @@ public class NormalCommentServiceImpl implements INormalCommentService {
     @Override
     public List<NormalComment> selectAll() {
         return normalCommentMapper.selectAll();
+    }
+
+    @Override
+    public List<Map<String, Object>> initNormalComment(CircleForFriendsCommentQueryObject qo) {
+        List<Map<String, String>> list = new ArrayList<>();
+        return normalCommentMapper.selectForInitNormalComment(qo);
     }
 }
